c105632d67d378fca77a1bba895c873be89e6f60
Grv25 <151151625+Grv25@users.noreply.github.com>
2025-06-07 18:57:10 +0300

    first commit

---
+++ UrlShortener/UrlDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Models;
+
+namespace UrlShortener
+{
+    public class UrlDbContext : DbContext
+    {
+        public UrlDbContext(DbContextOptions<UrlDbContext> options) : base(options) { }
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+    }
+}
