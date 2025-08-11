4a331837fb2f9c89d82b3c4f983752a9ef8c5d1c
Grv25 <151151625+Grv25@users.noreply.github.com>
2025-08-11 16:39:31 +0300

    Updated launchSettings file.

--- UrlShortener/obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
+++ UrlShortener/obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c105632d67d378fca77a1bba895c873be89e6f60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0173cae5818ead1e35f6c813660988955b341384")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
